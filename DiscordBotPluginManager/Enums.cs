b1672000a15d8e0e057dfb0fa81dc245aada64a6
Wizzy69 <45517207+Wizzy69@users.noreply.github.com>
2021-01-17 10:04:36 +0200

    Add files via upload

---
+++ DiscordBotPluginManager/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace DiscordBotPluginManager
+{
+    public enum ZipSearchType
+    {
+        ALL_TEXT,
+        CODE,
+    }
+
+    public enum SearchDirectory
+    {
+        RESOURCES,
+        CUSTOM
+    }
+
+    public enum PluginType
+    {
+        COMMAND,
+        ADDON
+    }
+}
