783aa99fdde321f06dabe16d68cb9f0f7d324737
Wizzy69 <45517207+Wizzy69@users.noreply.github.com>
2021-04-22 21:49:13 +0300

    Language System Updated

---
+++ DiscordBot/App/FirstTime/SettingsFirstRun.cs
@@ -0,0 +1,83 @@
+﻿using DiscordBotPluginManager;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordBot.App.FirstTime
+{
+    public partial class SettingsFirstRun : Form
+    {
+        public SettingsFirstRun()
+        {
+            InitializeComponent();
+
+            Load += (sender, e) => FormLoad();
+        }
+
+        private void FormLoad()
+        {
+
+            if (File.Exists(@".\Data\Resources\DiscordBotCore.data"))
+            {
+                textBox1.Text = Functions.readCodeFromFile("DiscordBotCore.data", SearchDirectory.RESOURCES, "BOT_TOKEN", '\t');
+                textBox2.Text = Functions.readCodeFromFile("DiscordBotCore.data", SearchDirectory.RESOURCES, "BOT_PREFIX", '\t');
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            foreach(var file in Directory.EnumerateFiles(Functions.langFolder))
+            {
+                string name = new FileInfo(file).Name;
+                string langName = Functions.readCodeFromFile(name, SearchDirectory.LANGUAGE, "LANGUAGE_NAME", '=');
+                comboBox1.Items.Add(langName);
+            }
+
+            if(comboBox1.Items.Count == 0)
+            {
+                DialogResult = DialogResult.Abort;
+                this.Close();
+            }
+
+
+
+            button1.Click += (sender, e) =>
+            {
+                if (textBox1.Text.Length < 50)
+                {
+                    MessageBox.Show("Invalid discord bot token !", "Discord Bot", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if(textBox2.Text.Length != 1)
+                {
+                    MessageBox.Show("Invalid Discord bot prefix !", "Discord Bot", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (comboBox1.SelectedItem.ToString() is null)
+                {
+                    MessageBox.Show("Invalid language selected", "Discord Bot", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                DialogResult = DialogResult.OK;
+
+                File.WriteAllText(@".\Data\Resources\DiscordBotCore.data",
+                    $"BOT_TOKEN\t{textBox1.Text}\nBOT_PREFIX\t{textBox2.Text}\n");
+                File.WriteAllText(@".\Data\Resources\DiscordBotSettings.data", "BotLanguage=" + comboBox1.SelectedItem.ToString());
+                this.Close();
+            };
+        }
+
+
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.IO;
+﻿
+using System;
 using System.Windows.Forms;
 
 namespace DiscordBot
 {
-	public class Program
-	{
-		/// <summary>
-		///     The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		[Obsolete]
-		public static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			AppDomain.CurrentDomain.AppendPrivatePath("Data\\lib");
-
-			Application.Run(new Form1());
-		}
-	}
+    public class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        [Obsolete]
+        public static void Main(string[] args) {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AppendPrivatePath("Data\\lib");
+            Application.Run(new Form1());
+        }
+    }
 }
